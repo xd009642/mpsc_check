8e4545f5b7b42463c00a77574b61476c10842022
xd009642 <danielmckenna93@gmail.com>
2024-05-20 12:27:26 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use tokio::sync::mpsc;
+
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(200);
+
+
+    let sender = async move {
+        for i in 0..1_000_000 {
+            println!("Sending: {}", i);
+            tx.send(i).await.unwrap();
+        }
+    };
+
+    let receiver = async move {
+        while let Some(val) = rx.recv().await {
+            println!("Receiving {:?}", val);
+        }
+    };
+
+    tokio::join!(receiver, sender);
+}
